87f4011232237744c001c2c393d31a38c9af5654
Sangramk33 <sangramkale4098@gmail.com>
2025-04-15 01:32:17 +0530

    bf' git push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,16 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+    proxy: process.env.NODE_ENV === 'development'
+      ? {
+          '/api': {
+            target: 'http://localhost:5000', // Local backend
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '')
+          }
+        }
+      : {}
   }
 });
